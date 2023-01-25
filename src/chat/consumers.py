db4ec132e05d664aa0bcef39aae3aa2856d69257
Hylke Nicola <hyskon@outlook.com>
2023-01-25 21:58:11 +0100

    Restyle+SerializeMsgs+BackendStorage

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -3,9 +3,51 @@ import json
 import os
 import openai
 from asgiref.sync import async_to_sync
+from django.contrib.auth import get_user_model
 from channels.generic.websocket import AsyncWebsocketConsumer
 from dotenv import load_dotenv
+from .models import Message
+
+User = get_user_model()
 class ChatConsumer(AsyncWebsocketConsumer):
+    async def fetch_messages(self, data):
+        messages = Message.last_20_messages()
+        content = {
+            'messages': self.messages_to_json(messages)
+        }
+        self.send_message(content)
+    
+    async def new_message(self, data):
+        author = data['from']
+        # TODO: Refactor this
+        author_user = User.objects.filter(username=author).first()
+        message = Message.objects.create(
+            author=author_user, 
+            content=data['message'])
+        content = {
+            'command': 'new_message',
+            'message': self.message_to_json(message)
+        }
+        return await self.send_chat_message(content)
+    
+    def messages_to_json(self, messages):
+        result = []
+        for message in messages:
+            result.append(self.message_to_json(message))
+        return result
+    
+    def message_to_json(self, message):
+        return {
+            'author': message.author.username,
+            'content': message.content,
+            'timestamp': str(message.timestamp)
+        }
+        
+    commands = {
+        "fetch_messages": fetch_messages,
+        "new_message": new_message,
+    }
+    
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
@@ -21,10 +63,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]        
-        # Send message to OpenAI
+    async def receive(self, text_data):        
+        data = json.loads(text_data)
+        await self.commands[data['command']](self, data)
+    
+    async def send_chat_message(self, message):
+        # TODO: make this conditional, so only when user input @GPT the openai is called
+        # # Send message to OpenAI        
         response = await self.send_to_openai(message)
         # build history
         self.history += message + "\n"
@@ -40,17 +85,20 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_send(
             self.room_group_name, {"type": "chat_message", "message": response}
         )
+    
+    async def send_message(self, message):
+        await self.send(text_data=json.dumps(message))
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event["message"]
-
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=json.dumps(message))
         await self.send(text_data=json.dumps({
             "command": "javascript",
             "code": f'document.getElementById("chat-log").value;'
-        }))
+        }))    
+    
     # Openai API call    
     async def send_to_openai(self, message):
         load_dotenv()
